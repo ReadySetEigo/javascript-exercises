d7f00e629d733ef987526c9611781a5277a0fb6e
Nermin <107202079+ReadySetEigo@users.noreply.github.com>
2025-03-20 18:37:51 +0000

    exercise 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, inputRepeat) {
+    if(inputRepeat < 0) return "ERROR";
+    let string = "";
+    for(let i = 0; i < inputRepeat; i++) {
+        string += inputString
+    }
+    return string;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let str = word;
+    splitStr = word.split("");
+    reverseSplit = splitStr.reverse();
+    reverseStr = reverseSplit.join("");
+    return reverseStr;
 };
 
 // Do not edit below this line
